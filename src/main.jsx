178cb52f52d4fb4d0bb153ae5a115baa25858c91
Jérôme DUPRAZ <dupraz.j@sfeir.com>
2023-04-21 11:14:59 +0200

    Add condition for prod mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,27 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reactToWebComponent from "react-to-webcomponent";
 import StarWars from "./StarWars";
 import PropTypes from "prop-types";
 
-StarWars.propTypes = {
-  title: PropTypes.string,
-  widgetid: PropTypes.string,
-};
+if (import.meta.env.PROD) {
+  StarWars.propTypes = {
+    title: PropTypes.string,
+    widgetid: PropTypes.string,
+  };
 
-customElements.define(
-  "react-starwars",
-  reactToWebComponent(StarWars, React, ReactDOM)
-);
+  customElements.define(
+    "react-starwars",
+    reactToWebComponent(StarWars, React, ReactDOM)
+  );
+} else {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
